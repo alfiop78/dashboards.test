a65873044f9aaf3015312feb86cfa3ea15f507f9
Alfio P <alfio.p78@gmail.com>
2019-11-19 16:06:17 +0100

    Creazione layout pagina e Dashboard con layout-0

--- dashboards/js/init.js
+++ dashboards/js/init.js
@@ -7,16 +7,102 @@
 var App = new Application();
 (() => {
   var app = {
-    Cube : new Cube(),
+    // Cube : new Cube(),
     Draw : null,
     report_id : 0,
-    reports : new Object()
+    reports : new Object(),
+    dialog : document.getElementsByTagName('dialog')[0],
+    activeSection : null // indica la sezione dove si è cliccato per l'inserimento di un oggetto
 
   };
 
   // App.getSessionName();
 
   App.init();
+
+  app.getDatamarts = function() {
+    /*recupero lista reports creati dal localStorage*/
+    // console.log(window.localStorage);
+    let reports = Object.keys(window.localStorage);
+    // console.log(reports);
+    // console.log(reports.length);
+    app.report_id = reports.length+1;
+    // console.log(app.report_id);
+    let ulDatamartList = document.getElementById('datamartList');
+    reports.forEach((report) => {
+      // console.log(JSON.parse(window.localStorage.getItem(report)));
+      let cube = JSON.parse(window.localStorage.getItem(report));
+      let li = document.createElement('li');
+      console.log(cube.type);
+      if (cube.type) {
+        li.id = cube.report_id;
+        li.innerHTML = report;
+        ulDatamartList.appendChild(li);
+        li.onclick = function(e) {
+          /*
+          1- Inserisco il nome del datamart(Report) selezionato in lyt-report
+          */
+          app.activeSection.querySelector('h5').innerText = li.innerHTML;
+          app.dialog.close();
+          // sezione già occupata dall'oggetto (report, chart, indicator, ecc...) appena selezionato
+          app.activeSection.setAttribute('associated-datamart', li.innerText);
+          app.activeSection.setAttribute('associated-datamart-id', li.id);
+        };
+      }
+
+
+    });
+
+    // data = window.localStorage.getItem('cube');
+
+  };
+
+  app.handlerAddObject = function(e) {
+    /*
+    Apro la dialog per selezionare l'oggetto (report o grafico, indicatori, ecc...) da incorporare nella pagina
+    */
+    console.log(this);
+    app.activeSection = e.path[2];
+
+    app.dialog.showModal();
+  };
+
+  app.getDatamart = function() {
+    /* Lista dei reportId (FXn) da recuperare*/
+    let reportId;
+    document.querySelectorAll('div[associated-datamart]').forEach((datamart) => {
+      console.log(datamart);
+      // TODO: implementare un'object json con la lista dei datamart da recuperare
+      // ... utilizzo un solo reportId per Test
+      reportId = +datamart.getAttribute('associated-datamart-id');
+    });
+    console.log(reportId);
+
+
+    var url = "ajax/reports.php";
+    let params = "reportId="+reportId;
+    // console.log(params);
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          var response = JSON.parse(request.response);
+          console.table(response);
+          app.createReport(response);
+
+        } else {
+
+        }
+      } else {
+
+      }
+    };
+
+    request.open('POST', url);
+    // request.setRequestHeader('Content-Type','application/json');
+    request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+    request.send(params);
+  };
 
 
   document.getElementById('mdc-next').onclick = function(e) {
@@ -29,6 +115,15 @@ var App = new Application();
     let pages = document.getElementById('pages');
     pages.setAttribute('data-step', page.getAttribute('data-step'));
     // app.getData();
+    // event su addObjectIcon nelle preview del dashboard
+    document.querySelectorAll('.page[selected] .addObjectIcon > i').forEach((btnAdd) => {btnAdd.onclick = app.handlerAddObject;});
+
+    if (+page.getAttribute('data-step') === 3 && page.hasAttribute('selected')) {
+      console.log('getDatamart');
+      // mi sto spostando sulla pagina dell'anteprima dashboard, recupero i datamart per questa pagina
+      app.getDatamart();
+    }
+
   };
 
   document.getElementById('mdc-back').onclick = function(e) {
@@ -49,5 +144,73 @@ var App = new Application();
 
   // event click supreview-layout
   document.querySelectorAll('.preview-layout').forEach((layout) => {layout.onclick = app.handlerPreviewLayoutSelected;});
+
+  app.getDatamarts();
+
+  app.createReport = function(response) {
+    console.log('create report');
+
+    let table = document.getElementById('table-01');
+
+    console.log(response);
+    // return;
+
+    let options =
+      {
+      'cols' : [
+        // {'col': 3, 'attribute': 'hidden'},
+        // {'col': 5, 'attribute': 'hidden'}
+
+      ],
+      'filters' : [
+        {'col': 0, 'attribute': 'multi'},
+        {'col': 1, 'attribute': 'multi'},
+        {'col': 3, 'attribute': 'hidden'}
+      ],
+      'metrics' : [2,3], // TODO: le metriche vanno nascoste nei filtri e formattate in modo diverso nella table
+      // 'title' : app.Cube.cube.name,
+      'inputSearch' : true // visualizzo e lego evento input alla casella di ricerca, in basso.
+      };
+    app.Draw = new Draw(table, options);
+    console.log(app.Draw);
+    // Opzione 1 - aggiungo tutte le colonne della query
+    Object.keys(response[0]).forEach((el, i) => {
+      // console.log('col:'+el);
+      app.Draw.addColumn(el, i);
+      // aggiungo un filtro per ogni colonna della tabella
+      app.Draw.addParams(el, i);
+    });
+    // Opzione 2 - aggiungo manualmetne le colonne
+    // app.Draw.addColumn('ID');
+    // app.Draw.addColumn('Dealer');
+
+    // aggiungo le righe
+    let arrParams = [];
+    for (let i in response) {
+      // console.log(Object.values(response[i]));
+      // Opzione 1 - Aggiunta colonne automaticamente (in base alla query)
+      app.Draw.addRow(Object.values(response[i]));
+      // TODO: eliminare gli spazi bianchi prima e/o dopo il testo
+      // Opzione 2 - Aggiunta colonne manualmente
+      // app.Draw.addRow([response[i].id, response[i].descrizione, response[i].versioneDMS, response[i].CodDealerCM]);
+    }
+
+    app.Draw.createDatalist();
+    // imposto, nel metodo draw, anche le options, per cui questa riga deve essere messa prima dell'aggancio degli eventi sulle input (sotto)
+    app.Draw.draw();
+
+    document.querySelectorAll('input[type="search"]:not([id="search"])').forEach((el) => {
+      el.oninput = app.handlerInput;
+      el.onclick = app.showFilters;
+      el.onblur = function(e) {
+        // console.log(e);
+        this.removeAttribute('placeholder');
+      };
+      let elementsSelected = Array.from(el.parentElement.querySelectorAll('.elements:not([multi]) > ul div.element'));
+
+      el.parentElement.querySelectorAll('.elements:not([multi]) > ul div.element').forEach((liElement) => {liElement.onclick = app.handlerSelect;});
+      el.parentElement.querySelectorAll('.elements[multi] > ul div.element').forEach((liElement) => {liElement.onclick = app.handlerSelectMulti;});
+    });
+  };
 
 })();
