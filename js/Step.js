81dca3ca6d20a064fe5718d45cefe11e861c0a82
alfiop78 <alfio.p78@gmail.com>
2020-02-28 18:11:13 +0100

    Step all'interno delle pagine

---
+++ js/Step.js
@@ -0,0 +1,33 @@
+/*
+timelineId = elemento id della class='timelineContent'
+*/
+
+class Steps {
+
+  constructor(stepTranslate) {
+    // definisco il div che deve effettuare la translate
+    console.log(stepTranslate);
+    this._translate = document.getElementById(stepTranslate);
+    this._page = document.querySelector('.step[selected]');
+    console.log(this._page);
+    console.log(this._page.offsetWidth);
+    this._pageWidth = this._page.offsetWidth + 32; // width della pagina da translare
+    console.log(this._pageWidth);
+
+  }
+
+  previous() {
+    // this._pageWidth = this._page.offsetWidth + 32;
+    this._translate.style.transform = "translateX("+(this._pageWidth-this._pageWidth)+"px)";
+  }
+
+  next() {
+    // this._pageWidth = this._page.offsetWidth + 32;
+    this._translate.style.transform = "translateX(-"+this._pageWidth+"px)";
+  }
+
+  goStep(e) {
+    
+  }
+
+}
